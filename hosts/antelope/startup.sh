7aa0e905545970d0a79b1603f2842f795d00aa5d
Adam W. Dace <colonelforbin74@gmail.com>
2019-06-12 13:27:28 -0400

    We longer run MySQL here.

--- hosts/antelope/startup.sh
+++ hosts/antelope/startup.sh
@@ -10,10 +10,10 @@
 echo "INFO: Install custom DNS settings..."
 sudo cp /etc/resolv.conf.custom /etc/resolv.conf
 
-echo "INFO: Starting MySQL..."
-sudo /etc/init.d/mysql.server start
+#echo "INFO: Starting MySQL..."
+#sudo /etc/init.d/mysql.server start
 
-sleep 1
+#sleep 1
 
 echo "INFO: Starting Apache HTTP..."
 sudo /usr/local/apache2/bin/apachectl start
