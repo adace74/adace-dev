6d6b561b6d262e00f62830635f45dd12dcf80317
Adam W. Dace <colonelforbin74@gmail.com>
2019-06-18 18:38:41 -0400

    Minor update.

--- hosts/contact/rsync_download.sh
+++ hosts/contact/rsync_download.sh
@@ -16,11 +16,37 @@ RSYNC_URL="rsync://mirror.bytemark.co.uk/dragonflybsd"
 LOCAL_DIR="/web/mirror.adamdace.info/dragonflybsd"
 
 echo "INFO:"
-echo "INFO: Downloading DFly ISO Images..."
+echo "INFO: Downloading DragonFly ISO Images..."
 echo "INFO:"
 
 cd $LOCAL_DIR/iso-images/
 $RSYNC $RSYNC_FLAGS $RSYNC_URL/iso-images/ .
+
+echo
+echo "----------------------------------------------------------------------"
+echo
+
+echo "INFO:"
+echo "INFO: Downloading DragonFly RELEASE Packages..."
+echo "INFO:"
+
+cd $LOCAL_DIR/dports/dragonfly:5.6:x86:64/
+$RSYNC $RSYNC_FLAGS $RSYNC_URL/dports/dragonfly:5.6:x86:64/ .
+
+echo
+echo "----------------------------------------------------------------------"
+echo
+
+echo "INFO:"
+echo "INFO: Downloading DragonFly DEVELOPMENT Packages..."
+echo "INFO:"
+
+cd $LOCAL_DIR/dports/dragonfly:5.8:x86:64/
+$RSYNC $RSYNC_FLAGS $RSYNC_URL/dports/dragonfly:5.8:x86:64/ .
+
+echo
+echo "----------------------------------------------------------------------"
+echo
 
 echo "INFO:"
 echo "INFO: Downloads complete."
