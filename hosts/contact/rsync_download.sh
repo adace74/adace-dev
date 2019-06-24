de98975902e57030fda38dc0833fec826609867c
Adam W. Dace <colonelforbin74@gmail.com>
2019-06-24 13:24:32 -0400

    Yup.

--- hosts/contact/rsync_download.sh
+++ hosts/contact/rsync_download.sh
@@ -11,16 +11,30 @@
 # Variables.
 #
 RSYNC="rsync"
-RSYNC_FLAGS="--archive --delete --verbose"
-RSYNC_URL="rsync://mirror.bytemark.co.uk/dragonflybsd"
+RSYNC_FLAGS="--archive --delete --ipv6 --verbose"
+#RSYNC_URL="rsync://avalon.dragonflybsd.org"
+#RSYNC_URL="rsync://mirror.bytemark.co.uk/dragonflybsd"
+RSYNC_URL="rsync://mirrors.coreix.net/mirrors/dragonflybsd"
 LOCAL_DIR="/web/mirror.adamdace.info/dragonflybsd"
 
 echo "INFO:"
 echo "INFO: Downloading DragonFly ISO Images..."
+date
 echo "INFO:"
 
 cd $LOCAL_DIR/iso-images/
 $RSYNC $RSYNC_FLAGS $RSYNC_URL/iso-images/ .
+
+echo
+echo "----------------------------------------------------------------------"
+echo
+
+echo "INFO:"
+echo "INFO: Downloading DragonFly Old RELEASE Packages..."
+echo "INFO:"
+
+cd $LOCAL_DIR/dports/dragonfly:5.4:x86:64/
+$RSYNC $RSYNC_FLAGS $RSYNC_URL/dports/dragonfly:5.4:x86:64/ .
 
 echo
 echo "----------------------------------------------------------------------"
@@ -38,17 +52,7 @@ echo "----------------------------------------------------------------------"
 echo
 
 echo "INFO:"
-echo "INFO: Downloading DragonFly DEVELOPMENT Packages..."
-echo "INFO:"
-
-cd $LOCAL_DIR/dports/dragonfly:5.8:x86:64/
-$RSYNC $RSYNC_FLAGS $RSYNC_URL/dports/dragonfly:5.8:x86:64/ .
-
-echo
-echo "----------------------------------------------------------------------"
-echo
-
-echo "INFO:"
 echo "INFO: Downloads complete."
+date
 echo "INFO:"
 echo "INFO: Done."
