f6900b56ee8a41c8a84ff92f675ec77e26707f3a
Adam W. Dace <colonelforbin74@gmail.com>
2019-12-12 21:17:00 -0800

    Yup.

--- hosts/antelope/startup.sh
+++ hosts/antelope/startup.sh
@@ -7,6 +7,9 @@
 #
 ######################################################################
 
+echo "INFO: Install custom DNS settings..."
+sudo cp /etc/resolv.conf.custom /etc/resolv.conf
+
 echo "INFO: Starting Apache HTTP..."
 sudo /usr/local/apache2/bin/apachectl start
 
